9419494906bdad591adf68fa920595ce03731a5d
kujun95 <kujun95@naver.com>
2019-08-06 16:15:17 +0900

    MemberDAO
    DAO

---
+++ MemberDAO.java
@@ -0,0 +1,113 @@
+package swing;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class MemberDAO {
+	private static MemberDAO instance;
+	
+	private String driver = "oracle.jdbc.driver.OracleDriver";
+	private String url = "jdbc:oracle:thin:@localhost:1521:xe";
+	private String user = "kujun";
+	private String password = "dkdlxl";
+	
+	private Connection conn;
+	private PreparedStatement pstmt;
+	private ResultSet rs;
+	
+	public static MemberDAO getInstance() {
+		if(instance == null){
+			synchronized (MemberDAO.class) {
+				instance = new MemberDAO();
+			}
+		}
+		return instance;
+	}
+	public MemberDAO() {
+		try {
+			Class.forName(driver);
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	public void getConnection() {
+		try {
+			conn = DriverManager.getConnection(url, user, password);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	public int login(MemberDTO dto) {
+		int su = 0;
+		getConnection();
+		String sql = "select from member where id = ?";
+		return su;
+	}
+	public int member(MemberDTO dto) {
+		System.out.println("연결 확인");
+		int su = 0;
+		getConnection();
+		String sql = "insert into member values(?,?,?,?,?,?,?,?,?,?)";
+		try {
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, dto.getId());
+			pstmt.setString(2, dto.getPw());
+			pstmt.setString(3, dto.getPwCheck());
+			pstmt.setString(4, dto.getName());
+			pstmt.setString(5, dto.getBirth());
+			pstmt.setString(6, dto.getEmail());
+			pstmt.setString(7, dto.getTel1());
+			pstmt.setString(8, dto.getTel2());
+			pstmt.setString(9, dto.getTel3());
+			pstmt.setString(10, dto.getAddress1());
+			
+			su = pstmt.executeUpdate();
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}finally {
+			try {
+				if(pstmt != null) pstmt.close();
+				if(conn != null) conn.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		return su;
+	}
+	public String searchID(MemberDTO dto) {
+		String id = null;
+		getConnection();
+		String sql = "select id from member where id = ?";
+		try {
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, dto.getId());
+			rs = pstmt.executeQuery();
+			while(rs.next()) {
+				id = rs.getString("id");
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}finally {
+				try {
+					if(rs != null) rs.close();
+					if(pstmt != null) pstmt.close();
+					if(conn != null) conn.close();
+				} catch (SQLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+		}
+		return id;
+	}
+	
+	
+}
